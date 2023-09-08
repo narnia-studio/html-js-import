a92486790436c73b04444893c8e66067825edc4b
dan-candeira <danielbarr16@gmail.com>
2023-09-08 12:04:19 -0300

    feat: add HTML import function + create webcomponent

---
+++ public/components/button.js
@@ -0,0 +1,25 @@
+class MyButton extends HTMLElement {
+	shadowRootRef;
+	counter = 0;
+
+	constructor() {
+		super();
+		this.shadowRootRef = this.attachShadow({ mode: "open" });
+	}
+
+	async connectedCallback() {
+		const { template } = await resolveHTMLImport("components/button.html");
+		this.shadowRootRef.appendChild(template.cloneNode(true));
+		
+		const button = this.shadowRootRef.getElementById("counter-button");
+		button.textContent = `Counter is ${this.counter}`;
+		button.addEventListener("click", () => {
+			this.counter++;
+			button.textContent = `Counter is ${this.counter}`;
+		});
+	}
+}
+
+if ("customElements" in window) {
+	customElements.define("my-button", MyButton);
+}
---
+++ public/utils/resolve-html-import.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @param {String} fileName
+ * @returns {HTMLElement}
+ */
+const resolveHTMLImport = (fileName) =>
+	new Promise(async (resolve, reject) => {
+		if (typeof fileName !== "string") {
+			reject("File name must be a string.");
+		}
+
+		try {
+			const file = await fetch(fileName).then((resp) => resp.text());
+			const parser = new DOMParser();
+			const htmlDoc = parser.parseFromString(file, "text/html");
+			const template =
+				htmlDoc.getElementsByTagName("template")[0].content;
+			resolve({ template });
+		} catch (error) {
+			reject("Error when trying to read file.");
+		}
+	});
